8e7a53ded0b837480ab334e12f1c23b87bb257ab
Murcyado <noah.caroux@gmail.com>
2023-02-15 09:01:48 +0100

    New day 3 Java Hibernate

--- bibliotheque/src/main/java/principale/Main.java
+++ bibliotheque/src/main/java/principale/Main.java
@@ -17,7 +17,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to our Library");
 		
-		
 		//System.out.println(categories);
 		
 		// Question 1 : Utiliser le CategoryDAO pour insérer une nouvelle catégorie dans la liste
